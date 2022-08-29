c07af5d4e88009243516fe70ccae9f8f8dbc635c
Kiptoy <88104909+Kiptoy@users.noreply.github.com>
2022-08-29 18:43:30 +0700

    Test2

--- minMedMax.js
+++ minMedMax.js
@@ -4,7 +4,7 @@ function minMedMax(n1, n2, n3) {
     arr = [n1 ,n2 ,n3]
     max = arr[0] > arr[1] ? ( (arr[0] > arr[2]) ? arr[0] : arr[2] ) : (arr[1] > arr[2]) ? arr[1] : arr[2] 
     min = arr[0] < arr[1] ? ( (arr[0] < arr[2]) ? arr[0] : arr[2] ) : (arr[1] < arr[2]) ? arr[1] : arr[2] 
-    for (num in arr) {
+    for (num of arr) {
         if (num != max && num != min) {
             mid = Number(num)
         }
