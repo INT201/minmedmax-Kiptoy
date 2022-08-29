a884e67fcddc520b09f0fffe2f403391b6aa880d
Kiptoy <88104909+Kiptoy@users.noreply.github.com>
2022-08-29 18:41:09 +0700

    Test 1

--- minMedMax.js
+++ minMedMax.js
@@ -1,6 +1,16 @@
 const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
-  //code here
-}
+    arr = [n1 ,n2 ,n3]
+    max = arr[0] > arr[1] ? ( (arr[0] > arr[2]) ? arr[0] : arr[2] ) : (arr[1] > arr[2]) ? arr[1] : arr[2] 
+    min = arr[0] < arr[1] ? ( (arr[0] < arr[2]) ? arr[0] : arr[2] ) : (arr[1] < arr[2]) ? arr[1] : arr[2] 
+    for (num in arr) {
+        if (num != max && num != min) {
+            mid = Number(num)
+        }
+    }
+    return {min : min,
+            mid : mid,
+            max : max}
+  }
 module.exports = minMedMax
